cf81c1b8a049c33424e37c5dc731b91dbe6b5dd2
Muthuraj Mohan kumar <mrohini116@gmail.com>
2023-01-27 13:58:02 +0000

    completed excercise 1

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,26 @@
-const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+const getPromiseResponse = async () => {
+  try{
+	const result = await getResult()
+	console.log("Yay! Promise resolved with response:" + result);
+  }
+  catch(err){
+	console.log("Boo. Promise rejected with response:" + err);
+  }
+};
+getPromiseResponse();
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
-});
+function getResult(){
+	return new Promise((resolve, reject) => {
+    // Generate random number between 0 and 9
+    const randomInt = Math.floor(Math.random() * 10);
 
-// Your solution(s) to exercise001 here!
+    if (randomInt % 2 === 0) {
+      // Success
+      setTimeout(() => resolve("done"), 2000);
+    } else {
+      // Failure
+      setTimeout(() => reject("error"), 2000);
+    }
+  });
+}
+
