6367662692ca5844172a36342e95745ec5aa7dcd
Muthuraj Mohan kumar <mrohini116@gmail.com>
2023-01-27 22:06:53 +0000

    completed task1 in ecercise 2

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,17 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+async function fetchData(apiEndPoint){
+  console.log(apiEndPoint);
+  let response = await fetch(apiEndPoint);
+  try{
+    const json = await response.json();
+    console.log(json);
+  }
+  catch(err){
+    console.log(err);
+  }
+  
 };
 
 fetchData(jsonTypicode);
